36c15f548ee6ddd8e2a7795edccb26619ae9dcad
jpcadinelli <jpcadinelli@gmail.com>
2023-11-03 16:47:52 -0300

    projeto concluído para desktop

--- advice-generator-app/script.js
+++ advice-generator-app/script.js
@@ -1,3 +1,10 @@
 function novoConselho() {
-    console.log('botão funcionando')
+    const url = 'https://api.adviceslip.com/advice'
+    fetch(url).then(response => { 
+            return response.json() 
+        }).then(adviceData => {
+            const adviceObj = adviceData.slip
+            document.querySelector('.numero-conselho').innerHTML = `#${adviceObj.id}`
+            document.querySelector('.conselho').innerHTML = `"${adviceObj.advice}"`
+        })
 }
